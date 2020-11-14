a2cc33bce6d33e6c5bbbb1f170a64628330b1ac2
zhounie <iszhounie@aliyun.com>
2020-11-14 16:38:36 +0800

    上传文件

---
+++ app.js
@@ -0,0 +1,51 @@
+const Koa = require('koa')
+const app = new Koa()
+const Router = require('koa-router')
+const router = Router() 
+const fs = require('fs')
+const path = require('path')
+const KoaBody = require('koa-body')
+const KoaStatic = require('koa-static')
+
+app.use(KoaBody({
+    multipart: true,
+    formidable: {
+        maxFileSize: 200 * 1024 * 1024
+    }
+}))
+
+app.use(KoaStatic(path.join(__dirname, '/data/')))
+
+router.post('/upload', async (ctx) => {
+    if (!ctx.request.files) {
+        return ctx.body = {
+            code: 99,
+            msg: '请上传文件，参数: file'
+        }
+    }
+    const { file } = ctx.request.files
+    if (!file) {
+        return ctx.body = {
+            code: 99,
+            msg: '请上传文件，参数: file'
+        } 
+    }
+    const name = `${new Date().getTime()}${file.name}`;
+    const reader = fs.createReadStream(file.path)
+    let filePath = path.resolve(__dirname, './data/images/', name)
+    const upStream = fs.createWriteStream(filePath)
+    reader.pipe(upStream)
+    return ctx.body = {
+        code: 200,
+        data: {
+            img: `http://81.69.28.107:6868/images/${name}`
+        }
+    }
+})
+
+app.use(router.routes())
+
+
+app.listen(6868, () => {
+  console.log('server listen: ' + 6868);  
+})
